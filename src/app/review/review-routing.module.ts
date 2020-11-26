e0c3fb7cd47497c7f13e532aa1d01c79f0716f54
Hristo-Neshev <62555171+Hristo-Neshev@users.noreply.github.com>
2020-11-26 13:56:49 +0200

    Add new components, routes and basic html
    Add components and routing for create, edit review, and basic html to them.

--- src/app/review/review-routing.module.ts
+++ src/app/review/review-routing.module.ts
@@ -1,13 +1,17 @@
 import { Routes, RouterModule } from '@angular/router';
 import { AllReviewsComponent } from './all-reviews/all-reviews.component';
+import { CreateComponent } from './create/create.component';
 import { DetailsComponent } from './details/details.component';
+import { EditComponent } from './edit/edit.component';
 
 
 const routes: Routes = [
-    {path:'review/allReviews', component: AllReviewsComponent},
+    { path: 'review/allReviews', component: AllReviewsComponent },
     { path: 'review/details/:id', component: DetailsComponent },
-    
-  
+    {path: 'review/create', component: CreateComponent},
+    {path: 'review/edit', component: EditComponent}
+
+
 ];
 
 export const ReviewRoutingModule = RouterModule.forChild(routes);
