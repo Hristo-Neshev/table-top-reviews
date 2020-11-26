7aee0201710afa06852513c45ddb38e52d7eeace
Hristo-Neshev <62555171+Hristo-Neshev@users.noreply.github.com>
2020-11-26 13:21:57 +0200

    Update header.component.ts

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  loggedIn = false;
+  loggedIn = true;
   constructor() { }
 
   ngOnInit(): void {
