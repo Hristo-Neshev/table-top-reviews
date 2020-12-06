c759481ce92eb1ebfaf9224af368d5ebe927f185
Hristo-Neshev <62555171+Hristo-Neshev@users.noreply.github.com>
2020-12-06 16:09:50 +0200

    Add dynamic information to user profile page.
    Add some information about user's account

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
+  user = null;
+  createdDate = null;
+  lastLogin = null;
   constructor() { }
 
   ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('userData'));
+    this.createdDate = new Date(this.user.created).toLocaleDateString();
+    this.lastLogin = new Date(this.user.lastLogin).toLocaleDateString();
+    console.log(this.user);
   }
 
 }
